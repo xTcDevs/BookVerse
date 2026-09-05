d633da6f4944a1db23458215cab7c838bb177f9c
xTcDev <obichukwudavid15@gmail.com>
2026-09-06 00:07:22 +0100

    refactor: simplify BookVerse application entry

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-import { getTheme, saveTheme } from './lib/storage.js';
 import './styles.css';
-
-function Root() {
-  const [dark, setDark] = useState(() => getTheme() === 'dark');
-
-  useEffect(() => {
-    saveTheme(dark ? 'dark' : 'light');
-  }, [dark]);
-
-  return (
-    <BrowserRouter>
-      <App dark={dark} setDark={setDark} />
-    </BrowserRouter>
-  );
-}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Root />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
