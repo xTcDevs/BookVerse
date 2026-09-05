d9bb8b07f1112df03aab47ee580f1d54a1228b44
xTcDev <obichukwudavid15@gmail.com>
2026-09-06 00:03:55 +0100

    refactor: add BookVerse page shell

---
+++ src/layouts/PageShell.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
+
+export default function PageShell({ savedCount, dark, onTheme }) {
+  return (
+    <div className={dark ? 'app dark' : 'app'}>
+      <Header savedCount={savedCount} dark={dark} onTheme={onTheme} />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
