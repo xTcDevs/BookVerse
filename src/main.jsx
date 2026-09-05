1d9bdc2d46e17fb4aa4359ea5a8ab0f7cb1fac63
xTcDev <obichukwudavid15@gmail.com>
2026-09-06 00:03:59 +0100

    refactor: add BrowserRouter root

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import { getTheme, saveTheme } from './lib/storage.js';
 import './styles.css';
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+function Root() {
+  const [dark, setDark] = useState(() => getTheme() === 'dark');
+
+  useEffect(() => {
+    saveTheme(dark ? 'dark' : 'light');
+  }, [dark]);
+
+  return (
+    <BrowserRouter>
+      <App dark={dark} setDark={setDark} />
+    </BrowserRouter>
+  );
+}
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+);
