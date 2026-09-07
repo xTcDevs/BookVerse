17f7e51d3a63de9785163b8b2015f6f44dab695d
xTcDev <obichukwudavid15@gmail.com>
2026-09-07 17:35:28 +0100

    refactor: add reusable search debounce hook

---
+++ src/hooks/useDebouncedValue.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from 'react';
+
+export function useDebouncedValue(value, delay = 180) {
+  const [debounced, setDebounced] = useState(value);
+
+  useEffect(() => {
+    const timer = window.setTimeout(() => setDebounced(value), delay);
+    return () => window.clearTimeout(timer);
+  }, [value, delay]);
+
+  return debounced;
+}
