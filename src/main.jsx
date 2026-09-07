3ce5fe551f187bd6106c2595414d31f1c4e0eabd
xTcDev <obichukwudavid15@gmail.com>
2026-09-07 17:42:02 +0100

    style: load BookVerse detail polish

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import ErrorBoundary from './components/ErrorBoundary.jsx';
 import './styles.css';
+import './styles/detail.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
